fad3949893351778ef79163c9eac72364d2b5ae4
Feno2105 <mamyrandria2105@gmail.com>
2026-02-06 13:21:05 +0300

    modification de la base pour implimenter l historique

--- backend/mon-projet-api/database/seeders/DatabaseSeeder.php
+++ backend/mon-projet-api/database/seeders/DatabaseSeeder.php
@@ -128,8 +128,7 @@ class DatabaseSeeder extends Seeder
                 'is_deleted' => false,
                 'budget' => 15000.50,
                 'surface' => 120.75,
-                'Id_entreprise' => 1,
-                'Id_signalement' => 1
+                'Id_entreprise' => 1
             ],
             [
                 'create_at' => $now,
@@ -137,8 +136,7 @@ class DatabaseSeeder extends Seeder
                 'is_deleted' => false,
                 'budget' => 8000.00,
                 'surface' => 60.50,
-                'Id_entreprise' => 2,
-                'Id_signalement' => 2
+                'Id_entreprise' => 2
             ]
         ]);
     }
